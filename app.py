105ca58713286fcbf294d2f17ec4b21de6de617b
ammarkhan99526-cloud <ammarkhan99526@gmail.com>
2025-08-12 13:27:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hy my code pipeline HELLO 123 CodePipeline Project and Successfully Running........!!! I\'m host %s' % socket.gethostname()
+    return 'hy my code pipeline HELLO 123 AMMAR HERE CodePipeline Project and Successfully Running........!!! I\'m host %s' % socket.gethostname()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
